f5dec9037c8d9cff007f9beed3eda0832ac492e6
AFSAR SHAIKH <42389542+shaikhshaaf@users.noreply.github.com>
2019-02-22 21:44:11 +0530

    fs.appendFile()  //creat a new file als fs.openFile() fs.writeFile() fs.updateFile() fs.uplinkFile()  //delete files fs.renameFile()

---
+++ node.js
@@ -0,0 +1,32 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('hello  shaaf welcome');
+}).listen(8080,console.log('running...'));
+
+
+
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use the url module to turn the querystring into an object:*/
+  var q = url.parse(req.url, true).query;
+  /*Return the year and month from the query object:*/
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
+
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url,true).query;
+	var txt = q.year +" "+ q.month;
+	res.end(txt);
+}).listen(8080);
